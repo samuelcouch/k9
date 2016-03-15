46378188ae752b5718ce5a7f54f0c5186a5be3a6
Sam Couch <samcouch91@gmail.com>
2016-03-14 21:32:44 -0400

    initial test

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,3 +1,8 @@
 jest.dontMock('../index')
 const watchdog = require('../index').default
 
+describe('k9', () => {
+    it('should have an empty queue when initiated', () => {
+        expect(watchdog.tasks.length).toBe(0)
+    })
+})
